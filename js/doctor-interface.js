41b5ffec05d7e4fed88a2c70e5564faf3875711d
rm <biffbangpow767@yahoo.com>
2017-10-22 15:01:49 -0700

    commit a new approach to an api call

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,35 +1,24 @@
 import { Find } from './../js/doctor.js';
 let apiKey = require('./../.env').apiKey;
 
-$(document).ready(function() {
-  let malladyFront = new Find();
-  $("#malladyButton").click(function(){
-    let mallady = $("#mallady").val();
-    $("#mallady").val("");
-    let promise = malladyFront.findDoctor(apiKey);
-    malladyFront.callApi(mallady, promise);
+  $(document).ready(function() {
+    let doctorFront = new Find();
+    $("#doctorButton").click(function(){
+      let doctor = $("#doctor").val();
+      $("#doctor").val("");
+      console.log(doctor);
+      let promise = doctorFront.search(apiKey);
+      doctorFront.callName(doctor, promise);
+      });
     });
-  });
 
-
-//   $("#doctorButton").click(function(){
-//     let doctorFront = new Find();
-//     let doctor = $("#doctor").val();
-//     console.log(doctor);
-//     doctorFront.findDoctor(doctor).then(outputDoctor);
-//     function outputDoctor(response){
-//       let body = JSON.parse(response);
-//       let arrays = [];
-//       body.data.forEach(function(doctor) {
-//         arrays.push(doctor);
-//         });
-//         arrays.forEach(function(doctor) {
-//             $('#doctorInfo').append(`<li>${doctor.phones}</li>`);
-//         }, function(error) {
-//             $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-//         });
-//         debugger;
-//       }
-//     });
-//
-// });
+    $(document).ready(function() {
+      let malladyFront = new Find();
+      $("#malladyButton").click(function(){
+        let mallady = $("#mallady").val();
+        $("#mallady").val("");
+        console.log(mallady);
+        let promise = malladyFront.search(apiKey);
+        malladyFront.callApi(mallady, promise);
+        });
+      });
--- js/doctor.js
+++ js/doctor.js
@@ -1,13 +1,16 @@
+
 let apiKey = require('./../.env').apiKey;
 
 export class Find {
   constructor() {
 
   }
-  findDoctor(doctor) {
+
+  search(query) {
       return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = "https://api.betterdoctor.com/2016-03-01/doctors?name=${`doctor`}location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&sort=last-name-asc&skip=0&limit=20&user_key=cbe5f7b04b5bb714d1411d4a9943f218";
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=cbe5f7b04b5bb714d1411d4a9943f218&query=${query}`;
+      console.log("here1");
         request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -20,23 +23,47 @@ export class Find {
     });
   }
 
-  callApi(mallady, promise) {
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      let arrays = [];
-      let arrays2 = [];
-      body.data.forEach(function(data) {
-        arrays.push(data);
-      });
-      arrays.forEach(function(practice) {
-        arrays2.push(practice)
-      });
-      arrays2.forEach(function(name) {
-      $("#malladyInfo").append(`${name.name}`);
-      }, function(error) {
-        $("#showErrors").text(`There was an error processing your request: ${error.message}`);
-      });
-    });
-  }
 
+  callName(query, promise, error) {
+   promise.then(function(response) {
+     let body = JSON.parse(response);
+     let data = body.data;
+     debugger;
+     console.log("here2");
+     data.forEach(function(nest) {
+       let doctorInfo = nest['doctorInfo'];
+       let doctorName = doctorInfo.first_name + ' ' + profile.last_name;
+       console.log(doctorName);
+      $('#doctorInfo').append("For Help With ${`query`}, contact: " + "<li>" + doctorName + "</li>")
+     }),
+   function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    };
+  });
+
+}
+
+
+  //  if(query === this.query) {
+  //    arrays.forEach(function(query) {
+  //      $("#doctorInfo").append(query.practices[0].first_name);
+  //    });
+  //     }
+    //   else {
+    //    return "Sorry, There Are No Doctors That Match Your Criteria";
+    //  }
+
+
+
+
+//  getDoctorByName(name) {
+//    if(name===this.name){
+//      arrays.forEach(function(name){
+//        $("#doctorInfo").text(name.practices[0].name)
+//      } else {
+//        "Sorry, There Are No Doctors That Match Your Criteria"
+//      })
+//    }
+//  }
+//
 }
