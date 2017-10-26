a20338d2f7c036e8a110ad9f02b358b9ec57a94d
rm <biffbangpow767@yahoo.com>
2017-10-25 20:17:48 -0700

    fulfill all requirements for the doctor code review

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,26 +1,23 @@
 import { Find } from './../js/doctor.js';
-let apiKey = require('./../.env').apiKey;
 
   $(document).ready(function() {
     let doctorFront = new Find();
     $("#doctorButton").click(function(){
       let doctor = $("#doctor").val();
       $("#doctor").val("");
-      console.log(doctor);
       let query = "name=" + doctor;
-      let promise = doctorFront.search(apiKey, query);
-      doctorFront.listDoctors(doctor, promise);
+      let promise = doctorFront.search(query);
+      doctorFront.callName(query, promise);
       });
     });
 
     $(document).ready(function() {
       let malladyFront = new Find();
-      $("#malladyButton").click(function(){
+      $("#malladyButton").click(function() {
         let mallady = $("#mallady").val();
         $("#mallady").val("");
-        console.log(mallady);
         let query = "query=" + mallady;
-        let promise = malladyFront.search(apiKey, query);
-        malladyFront.listDoctors(mallady, promise);
+        let promise = malladyFront.search(query);
+        malladyFront.callName(query, promise);
         });
       });
--- js/doctor.js
+++ js/doctor.js
@@ -9,7 +9,7 @@ export class Find {
   search(query) {
       return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}&${query}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_key=${apiKey}&${query}`;
 
         request.onload = function() {
         if (this.status === 200) {
@@ -28,35 +28,30 @@ export class Find {
    promise.then(function(response) {
      let body = JSON.parse(response);
      let data = body.data;
-     data.forEach(function(practice) {
-      let profile = practice.profile;
-      let name = profile.first_name + ' ' + profile.last_name;
-      $("#doctorInfo").show().append('<li>Name: ' + name + '</li><br>');
-      let phoneNumbers = practice.practices[0].phones;
-      let phoneNumber = phoneNumbers[0].number;
-      $("#doctorInfo").show().append('<li>Phone Number: ' + phoneNumber + '</li><br>');
-      let addresses = practice.practices[0].visit_address;
-      let address = addresses.street + ' ' + addresses.city + ' ' + addresses.state + ' ' + addresses.zip;
-      $("#doctorInfo").show().append('<li>Address: ' + address + '</li><br>');
-      let website = practice.practices[0].website;
-      $("#doctorInfo").show().append('<li>Website: ' + website + '</li><br>');
-      let acceptsNew = practice.practices[0].accepts_new_patients;
-      let info = $("#doctorInfo").show().append('<li>Accepting New Patients? ' + acceptsNew + '</li><br><hr>');
-      debugger;
-      }),
-      function(error) {
-        $('.showErrors').show()(`There was an error processing your request: ${error.message}`);
-      };
+     if (data.length > 0) {
+       $("#doctorInfo").empty();
+       data.forEach(function(practice) {
+        let profile = practice.profile;
+        let name = profile.first_name + ' ' + profile.last_name;
+        $("#doctorInfo").append('<li>Name: ' + name + '</li><br>');
+        let phoneNumbers = practice.practices[0].phones;
+        let phoneNumber = phoneNumbers[0].number;
+        $("#doctorInfo").append('<li>Phone Number: ' + phoneNumber + '</li><br>');
+        let addresses = practice.practices[0].visit_address;
+        let address = addresses.street + ' ' + addresses.city + ' ' + addresses.state + ' ' + addresses.zip;
+        $("#doctorInfo").append('<li>Address: ' + address + '</li><br>');
+        let website = practice.practices[0].website;
+        $("#doctorInfo").append('<li>Website: ' + website + '</li><br>');
+        let acceptsNew = practice.practices[0].accepts_new_patients;
+        $("#doctorInfo").append('<li>Accepting New Patients? ' + acceptsNew + '</li><br><hr>');
+        debugger;
+        }),
+        function(error) {
+          $('.showErrors')(`There was an error processing your request: ${error.message}`);
+        };
+      } else {
+        $("#doctorInfo").append("Your Query Returned No Results, Try Again!");
+      }
     });
-  }
-
-  listDoctors(query, promise) {
-    let body = JSON.parse(response);
-
-    if (query === body.data.practices.profile.first_name || query === body.data.practices.profile.first_name || query === body.data.practices.profile.first_name) {
-    query.callName();
-    } else {
-      "Sorry, your query matched no doctors. Please try again."
-    }
   }
 }
